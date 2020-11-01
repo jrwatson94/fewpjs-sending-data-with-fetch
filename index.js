dacf1a436bfc10296fe2912affe24c53e9585ceb
jrwatson94 <j.watsonreid@gmail.com>
2020-11-01 17:26:55 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name,email){
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": name,
+            "email": email
+        })
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        document.body.append(data.id)
+    })
+    .catch(function(error) {
+        document.body.append(error.message);
+    }) 
+};
+
+
+
